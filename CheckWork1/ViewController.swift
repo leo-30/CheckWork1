d9e74b2c1ed9fcf909cf3c43575acbdee3633ded
mio <mio.rabbit971.snow@gmail.com>
2020-04-03 14:37:44 +0900

    2020春れお班CheckWork1解答

--- CheckWork1/ViewController.swift
+++ CheckWork1/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus() {
+        number -= 1
+        label.text = String(number)
+    }
+    
+    @IBAction func kakeru() {
+        number *= 2
+        label.text = String(number)
+    }
+    
+    @IBAction func waru() {
+        number /= 2
+        label.text = String(number)
+    }
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+    }
 
 }
 
